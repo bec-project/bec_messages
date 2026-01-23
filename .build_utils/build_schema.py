fc9f532ebf3f8d584f83c8a16aca0206bc9d41f0
perl_d <david.perl@psi.ch>
2026-01-23 09:24:35 +0100

    wip get refs to subschema

--- .build_utils/build_schema.py
+++ .build_utils/build_schema.py
@@ -1,14 +1,17 @@
 import argparse
+from enum import Enum
+import inspect
 import json
 import os
 from pathlib import Path
-from typing import Callable, Iterable, Literal
+from typing import Any, Callable, Iterable, Literal, Self, get_args, get_origin
 import bec_messages
 import bec_messages.messages
 from pydantic import BaseModel
 
 
-def _get_model_classes():
+def _get_model_classes() -> Iterable[type[BaseModel]]:
+    """Get all the relevant classes from the message definitions."""
     return (
         cls
         for cls in bec_messages.messages.__dict__.values()
@@ -16,6 +19,72 @@ def _get_model_classes():
         and cls is not BaseModel
         and not cls.__name__.startswith("_")
     )
+
+
+ItemType = BaseModel | Enum
+
+
+class GraphNode:
+    def __init__(self, model: type[ItemType]):
+        self.model = model
+        self.children: set[GraphNode] = set()
+
+    def add_child(self, child: Self):
+        self.children.add(child)
+
+    def __repr__(self):
+        return f"GraphNode({self.model.__name__})"
+
+
+def extract_from_anno(tp: type[Any] | None) -> set[type[ItemType]]:
+    """Return all the BaseModel subclasses in an annotation."""
+    if tp is None:
+        return set()
+    if inspect.isclass(tp) and issubclass(tp, get_args(ItemType)):
+        return {tp}
+
+    args = get_args(tp)
+    result = set()
+    for arg in args:
+        result |= extract_from_anno(arg)
+    return result
+
+
+def extract_child_models(model: type[ItemType]):
+    """Get all child models for a model"""
+    if not issubclass(model, BaseModel):
+        return set()
+    result = set()
+    for _, info in model.model_fields.items():
+        result |= extract_from_anno(info.annotation)
+
+
+class ModelGraph:
+    def __init__(self, models: Iterable[type[BaseModel]]):
+        self.nodes: dict[type[ItemType], GraphNode] = {}
+        for model in models:
+            self._process_model(model)
+
+    def get_node(self, model: type[ItemType]) -> GraphNode:
+        if model not in self.nodes:
+            self.nodes[model] = GraphNode(model)
+        return self.nodes[model]
+
+    def _process_model(self, model: type[BaseModel]):
+        parent_node = self.get_node(model)
+
+        for field in model.model_fields.values():
+            referenced_models = extract_from_anno(field.annotation)
+
+            for ref_model in referenced_models:
+                child_node = self.get_node(ref_model)
+                parent_node.add_child(child_node)
+
+    def roots(self) -> set[GraphNode]:
+        all_children = {
+            child for node in self.nodes.values() for child in node.children
+        }
+        return set(self.nodes.values()) - all_children
 
 
 def _json_schema_dir():
@@ -55,9 +124,23 @@ def _check():
     """Check that the contents of the built schema would be unchanged by a rebuild"""
 
 
-Actions = Literal["check", "rebuild"]
+def _display_graph():
+    "print a graph of the relationships of the models"
+    clss = list(_get_model_classes())
+    for cls in clss:
+        cls.model_rebuild()
+    dag = ModelGraph(clss)
+    for node in dag.nodes.values():
+        print(node, "->", node.children)
 
-SUBCOMMANDS: dict[Actions, Callable] = {"check": _check, "rebuild": _rebuild}
+
+Actions = Literal["check", "rebuild", "display"]
+
+SUBCOMMANDS: dict[Actions, Callable] = {
+    "check": _check,
+    "rebuild": _rebuild,
+    "display": _display_graph,
+}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
